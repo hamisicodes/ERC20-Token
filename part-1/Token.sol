b81473a86dec58d4ba6bf3ea5a8ffec588fdb05b
Hamisi Ngao <hamisi.n@code-care.pro>
2022-06-14 15:50:05 +0300

    implement other functions

--- part-1/Token.sol
+++ part-1/Token.sol
@@ -4,32 +4,47 @@ pragma solidity ^0.8.7;
 contract Token {
 
     uint public totalSupply;
+    mapping (address => uint) public balance;
+    mapping (address => mapping(address => uint)) public approvals;
     event Transfer(address sender, address recepient, uint amount);
     event Approval(address owner, address spender, uint amount);
 
     /**
      * @dev creates new tokens.total supply is then increased
      */
-    function createTokens() public {
-        // code here 
-
+    function mint(uint amount) public {
+        balance[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
     }
 
     /**
-     * @dev transfer amount from sender to recepient
+     * @dev transfer amount from caller's account to recepient
+     * Returns true If transfer was done
+     */
+    function transfer(address recepient, uint amount) public returns (bool) {
+        balance[msg.sender] -= amount;
+        balance[recepient] += amount;
+        return true;
+    }
+
+    /**
+     * @dev transfer amount from sender's account to recepient
      * Returns true If transfer was done
      */
     function transferFrom(address sender, address recepient, uint amount) public returns (bool) {
-        // code here
+        balance[sender] -= amount;
+        balance[recepient] += amount;
+        return true;
     }
 
     /**
      * @dev approve sending of tokens on the owner's behalf by the sender
      * Returns true if approved
      */
-    function approve(address owner, address spender, uint amount) public returns (bool) {
-        // code here
-
+    function approve(address spender, uint amount) public returns (bool) {
+        approvals[msg.sender][spender] = amount;
+        return true;
     }
 
 
