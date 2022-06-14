86e8d50bd2109a4f9272c37680b995e9d2497070
Hamisi Ngao <hamisi.n@code-care.pro>
2022-06-14 13:13:48 +0300

    function setup and documentation

--- part-1/Token.sol
+++ part-1/Token.sol
@@ -2,5 +2,35 @@
 pragma solidity ^0.8.7;
 
 contract Token {
-    
+
+    uint public totalSupply;
+    event Transfer(address sender, address recepient, uint amount);
+    event Approval(address owner, address spender, uint amount);
+
+    /**
+     * @dev creates new tokens.total supply is then increased
+     */
+    function createTokens() public {
+        // code here 
+
+    }
+
+    /**
+     * @dev transfer amount from sender to recepient
+     * Returns true If transfer was done
+     */
+    function transferFrom(address sender, address recepient, uint amount) public returns (bool) {
+        // code here
+    }
+
+    /**
+     * @dev approve sending of tokens on the owner's behalf by the sender
+     * Returns true if approved
+     */
+    function approve(address owner, address spender, uint amount) public returns (bool) {
+        // code here
+
+    }
+
+
 }
